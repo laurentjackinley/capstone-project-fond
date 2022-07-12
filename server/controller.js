028f1e8600eb1a2772adae8689404872afbe80ed
Jackinley Laurent <laurentjackinley97@gmail.com>
2022-07-12 16:06:33 -0600

    ready1

--- server/controller.js
+++ server/controller.js
@@ -3,13 +3,6 @@ const parks = require('./db.json')
 
 let globalId = 7
 module.exports = {
-    // getPlace: (req, res) => {
-    //     const places = ['Zion National Park, UT','Acrches National Park,UT','Death Valley National Park, NV','Sequoia National Park, CA','Yellowstone National Park, ID']
-    //     let randomIndex = Math.floor(Math.random() * places.length);
-    //     let randomPlace = places[randomIndex];
-
-    //     res.status(200).send(randomPlace);
-    // },
     
     getParks:  (req, res) => {
         res.status(200).send(parks)
